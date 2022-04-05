19f6e454b82af572e5679f29027f279eec437a6a
Gleb <g.t.ru@mail.ru>
2022-04-05 10:12:55 +0300

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import dbData from './db.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Angularproject';
+
+  public users:{_id:String,amount:Number,type:String,name:{first: String,last: String},
+    company: String,
+    email: String,
+    phone: String,
+    address: String}[] = dbData;
+  
+  constructor(
+  ) { }
 }
